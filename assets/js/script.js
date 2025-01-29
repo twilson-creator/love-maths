498b116ae96cc2a6ebbc1378273c2aa1f7eb85bb
twilson-creator <panopticon2015@gmail.com>
2025-01-29 21:32:19 +0000

    Add score tracking functionality to answer checking logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,8 +34,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert('Hey! You got it right! :D');
+        incrementScore();  
     } else {
         alert(`Awwww...you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     
     runGame(calculatedAnswer[1]);
@@ -57,6 +59,9 @@ function calculateCorrectAnswer() {
 }
 
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 
 }   
 
