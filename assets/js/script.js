391e8b40024c63e83b4d86da86fbdc3b992dc6f3
twilson-creator <panopticon2015@gmail.com>
2025-01-29 18:34:47 +0000

    Enhance runGame function to accept game type and handle addition questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,17 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer() {
@@ -34,8 +42,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
